e9e94d53f46a06226975c1318d0fa2a4ec372efc
ManuelLovell <69539934+ManuelLovell@users.noreply.github.com>
2024-01-28 22:42:26 +0000

    Build: (64f7cc3) AdvancedNotation

---
+++ assets/dicenotify-kWTVPdXs.js
@@ -0,0 +1,9 @@
+import{O as c,C as e}from"./bsConstants-E4N5t5Rc.js";/* empty css                   */c.onReady(async()=>{const d=window.location.search,t=new URLSearchParams(d),a=decodeURIComponent(t.get("sender")),r=decodeURIComponent(t.get("message")),s=decodeURIComponent(t.get("color")),i=decodeURIComponent(t.get("queue"));e.BONESNOTIFICATIONS.innerHTML=`
+        <div style="color:${s};" id="diceSender">${a}</div>
+        <div id="diceContainer">
+            <div id="diceImage">
+                <img id="diceSvg" src="/dice-twenty.svg" alt="Dice Image">
+            </div>
+            <div id="diceMessage" class="dice-notification">${r}</div>
+        </div>
+    `;const n=document.getElementById("notifyHtml");e.BONESNOTIFICATIONS.style.border=`1px solid ${s}`;const o=document.createElement("input");o.type="image",o.classList.add("close-notify-button"),o.src="/close-button.svg",o.onclick=async()=>{await c.popover.close(e.EXTENSIONNOTIFY+i)},e.BONESNOTIFICATIONS.appendChild(o),n.scrollHeight>n.clientHeight&&await c.popover.setHeight(e.EXTENSIONNOTIFY+i,n.scrollHeight),setTimeout(async()=>{await c.popover.close(e.EXTENSIONNOTIFY+i)},3e3)});
--- assets/dicenotify-tC9Po3vv.js
+++
@@ -1,9 +0,0 @@
-import{O as c,C as e}from"./bsConstants-E4N5t5Rc.js";c.onReady(async()=>{const d=window.location.search,t=new URLSearchParams(d),a=decodeURIComponent(t.get("sender")),r=decodeURIComponent(t.get("message")),i=decodeURIComponent(t.get("color")),n=decodeURIComponent(t.get("queue"));e.BONESNOTIFICATIONS.innerHTML=`
-        <div style="color:${i};" id="diceSender">${a}</div>
-        <div id="diceContainer">
-            <div id="diceImage">
-                <img id="diceSvg" src="/dice-twenty.svg" alt="Dice Image">
-            </div>
-            <div id="diceMessage">${r}</div>
-        </div>
-    `;const s=document.getElementById("notifyHtml");e.BONESNOTIFICATIONS.style.border=`1px solid ${i}`;const o=document.createElement("input");o.type="image",o.classList.add("close-notify-button"),o.src="/close-button.svg",o.onclick=async()=>{await c.popover.close(e.EXTENSIONNOTIFY+n)},e.BONESNOTIFICATIONS.appendChild(o),s.scrollHeight>s.clientHeight&&await c.popover.setHeight(e.EXTENSIONNOTIFY+n,s.scrollHeight),setTimeout(async()=>{await c.popover.close(e.EXTENSIONNOTIFY+n)},3e3)});
