e365c9d60e40123cb4e271009552019f03a08046
ManuelLovell <69539934+ManuelLovell@users.noreply.github.com>
2024-01-28 22:55:37 +0000

    Build: (1f7c464) TextureSafety

--- assets/whatsnew-VULdYemN.js
+++ assets/whatsnew-VULdYemN.js
@@ -1,4 +1,4 @@
-import{O as e,C as o}from"./bsConstants-E4N5t5Rc.js";const s=document.querySelector("#bs-whatsnew"),r=document.querySelector("#bs-whatsnew-notes");s.innerHTML=`
+import{O as e,C as o}from"./bsConstants-mrMMLsLa.js";const s=document.querySelector("#bs-whatsnew"),r=document.querySelector("#bs-whatsnew-notes");s.innerHTML=`
   <div id="newsContainer">
     <h1>Bones! 1/26</h1>
     </br> So, some things shuffled around. I decided to use the old dice-box package that Rumble started with for this one. It seems to have better performance in areas, though loses in some other areas.
