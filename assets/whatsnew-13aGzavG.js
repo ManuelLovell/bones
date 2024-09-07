6ddab746e1e83187668a1c80addc6103c601dec4
ManuelLovell <69539934+ManuelLovell@users.noreply.github.com>
2024-09-07 19:12:39 +0000

    Build: (db527fd) update: accessibility

--- assets/whatsnew-13aGzavG.js
+++ assets/whatsnew-13aGzavG.js
@@ -1,5 +1,13 @@
-import{O as e,C as o}from"./bsConstants-uz3BotTD.js";const n=document.querySelector("#bs-whatsnew"),s=document.querySelector("#bs-whatsnew-notes");n.innerHTML=`
+import{O as e,C as t}from"./bsConstants-anvcUbPb.js";const s=document.querySelector("#bs-whatsnew"),n=document.querySelector("#bs-whatsnew-notes");s.innerHTML=`
   <div id="newsContainer">
+    <h1>Bones! 9/7</h1>
+    Just some accessibility updates today.
+    </br> Added a dice zoom option, so you can have mondo-dice. Or min-dice if you're insane.
+    </br> I wanted to add front-coloring in (like Bones) but I'll have to tinker some more to see if that's possible with this dice package.
+    </br> Also some styling updates for people using zoomed-in displays, or have an arrow vertical screen.  The scrollbar/scrolling should be available now in case.
+    </br> I do see that Genesys dice are available for the package now, so I'll probably look into getting those configured soon.
+    </br> Enjoy!
+    </br>
     <h1>Bones! 5/24</h1>
     </br> Minor update to fix how negative modifiers are shown in the log (1d20-5, for example was showing as a positive in the roll text).
     <h1>Bones! 5/24</h1>
@@ -27,11 +35,11 @@ import{O as e,C as o}from"./bsConstants-uz3BotTD.js";const n=document.querySelec
     </br> Also took a layer off the 'hidden' dice rolls. Now it lets people know you rolled (As was suggested by a Discord member), but doesn't tell them the results.
     </br>
   </div>
-`;e.onReady(async()=>{s.innerHTML=`
+`;e.onReady(async()=>{n.innerHTML=`
     <div id="footButtonContainer">
         <a href="https://www.patreon.com/battlesystem" target="_blank">Patreon!</a>
         <a href="https://discord.gg/ANZKDmWzr6" target="_blank">OBR Discord!</a>
         <a href="https://help.roll20.net/hc/en-us/articles/360037773133-Dice-Reference" target="_blank">Roll20 Dice Reference</a>
     </div>
     <div class="close"><img style="height:40px; width:40px;" src="/close-button.svg"></div>
-    `;const t=document.querySelector(".close");t.onclick=async()=>{await e.modal.close(o.EXTENSIONWHATSNEW)}});
+    `;const o=document.querySelector(".close");o.onclick=async()=>{await e.modal.close(t.EXTENSIONWHATSNEW)}});
